2d6bb71761eda8fa4bf76171d370b00a2efe65fd
Mosabesmael <mosabesmael123456789@gmail.com>
2026-07-08 19:04:47 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using TaskManagementAPI.Models;
+using TaskManagementAPI.Repositories;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// اختبار 1: Integers
+var intRepo = new Repository<int>();
+intRepo.Add(10);
+intRepo.Add(20);
+intRepo.Add(30);
+intRepo.PrintAll();
+
+// اختبار 2: Strings
+var stringRepo = new Repository<string>();
+stringRepo.Add("Task 1");
+stringRepo.Add("Task 2");
+stringRepo.PrintAll();
+
+// اختبار 3: Tasks (الأهم)
+var taskRepo = new Repository<TaskItem>();
+
+var task1 = new TaskItem
+{
+    Id = 1,
+    Title = "Learn Generics",
+    Description = "understand Generic Classes",
+    IsCompleted = true ,
+    CreatedAt = DateTime.Now,
+    DueDate = DateTime.Now.AddDays(2)
+};
+
+var task2 = new TaskItem
+{
+    Id = 2,
+    Title = "build Repository",
+    Description = "creat Generic Repository",
+    IsCompleted = true,
+    CreatedAt = DateTime.Now,
+    DueDate = DateTime.Now.AddDays(3)
+};
+
+taskRepo.Add(task1);
+taskRepo.Add(task2);
+taskRepo.PrintAll();
+
+Console.WriteLine($"\nnumbers of tasks: {taskRepo.Count}");
---
+++ Repositories/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagementAPI.Repositories;
+
+public class Repository<T>
+{
+    private List<T> _items = new();
+
+    public void Add(T item)
+    {
+        _items.Add(item);
+    }
+
+    public T GetByIndex(int index)
+    {
+        return index >= 0 && index < _items.Count ? _items[index] : default;
+    }
+
+    public List<T> GetAll()
+    {
+        return new List<T>(_items);
+    }
+
+    public int Count => _items.Count;
+
+    public void PrintAll()
+    {
+        Console.WriteLine($"\n=== {typeof(T).Name}s ({_items.Count}) ===");
+        for (int i = 0; i < _items.Count; i++)
+        {
+            Console.WriteLine($"{i}. {_items[i]}");
+        }
+    }
+}
